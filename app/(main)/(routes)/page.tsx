455bbe31a29425bff139f099ccca1f2190919acc
Iurii <furmaniurii@gmail.com>
2023-12-18 15:58:46 +0700

    feat: set up project

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/mode-toggle";
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
+    </div>
+  );
+}
